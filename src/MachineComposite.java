055d5126f7630557ffb57bacf9361dcc22bfd0de
edu94 <eduard.robinat@gmail.com>
2019-06-16 19:23:04 +0200

    apartat c

--- src/MachineComposite.java
+++ src/MachineComposite.java
@@ -1,25 +1,67 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
 
-public class MachineComposite extends MachineComponent{
+public class MachineComposite extends MachineComponent implements Observer {
 
     private List<MachineComponent> components = new ArrayList<>();
+    private int brokenComponents = 0;
+    private boolean broken = false;
 
     public void addComponent(MachineComponent mc) {
         components.add(mc);
-    }
-
-    @Override
-    public boolean isBroken() {
-        if(broken){
-            return true;
-        }
-        for(MachineComponent mc: components) {
-            if (mc.isBroken()){
-                return true;
+        mc.addObserver(this);
+        if (mc.isBroken()) {
+            brokenComponents += 1;
+            if (!broken && brokenComponents == 1) {
+                notifyChanges();
             }
         }
-        return false;
+    }
+    public boolean isBroken () {
+        return broken || brokenComponents > 0;
+    }
+
+    public void setBroken () {
+        boolean wasBroken = isBroken();
+        broken = true;
+        if (!wasBroken) {
+            notifyChanges();
+        }
+    }
+    public void repair () {
+        boolean wasBroken = isBroken();
+        broken = false;
+        if (!wasBroken) {
+            notifyChanges();
+        }
+    }
+    public void update (Observable obs, Object arg){
+        MachineComponent mc = (MachineComponent) obs;
+        if (mc.isBroken()) {
+            brokenComponent();
+        } else {
+            repairedComponent(mc);
+        }
+    }
+    private void brokenComponent () {
+        boolean wasBroken = isBroken();
+        brokenComponents += 1;
+        if (!wasBroken) {
+            notifyChanges();
+        }
+    }
+    private void repairedComponent (MachineComponent mc){
+        brokenComponents -= 1;
+        if (!isBroken()) {
+            notifyChanges();
+        }
+    }
+
+    private void notifyChanges () {
+        setChanged();
+        notifyObservers();
     }
 }
